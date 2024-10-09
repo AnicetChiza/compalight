7578403669816146ba4c9b56b5f89639491cb302
AnicetChiza <chizaanicet@gmail.com>
2024-10-09 13:19:13 +0200

    add to my repository

---
+++ asset/js/app.js
@@ -0,0 +1,9 @@
+// const lightEffect = document.querySelector('.light-effect');
+
+// document.addEventListener('mousemove', (e) => {
+//     const x = (e.clientX / window.innerWidth) * 100;
+//     const y = (e.clientY / window.innerHeight) * 100;
+
+//     lightEffect.style.top = `${y}%`;
+//     lightEffect.style.left = `${x}%`;
+// });
