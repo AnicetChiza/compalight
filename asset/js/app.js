e8ca681cdfa186a7183e10f0a1a01623cfee09e7
AnicetChiza <chizaanicet@gmail.com>
2024-10-09 13:55:43 +0200

    add to my repository

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,9 +1,36 @@
-// const lightEffect = document.querySelector('.light-effect');
+// const texts = ["Votre agence!", "Communiquer pour inpirer!", "Unir pour prosperer!", "Impacter pour briller!"];
+const texts = ["Bienvenue!", "Explorez nos services!", "Contactez-nous!"];
+let index = 0;
+let charIndex = 0;
+let currentText = '';
+let isDeleting = false;
 
-// document.addEventListener('mousemove', (e) => {
-//     const x = (e.clientX / window.innerWidth) * 100;
-//     const y = (e.clientY / window.innerHeight) * 100;
+function type() {
+    const fullText = texts[index];
 
-//     lightEffect.style.top = `${y}%`;
-//     lightEffect.style.left = `${x}%`;
-// });
+    if (isDeleting) {
+        currentText = fullText.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        currentText = fullText.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    document.getElementById("text").textContent = currentText;
+
+    if (!isDeleting && charIndex === fullText.length) {
+        setTimeout(() => {
+            isDeleting = true;
+        }, 1000); // Temps d'attente avant de commencer à effacer
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        index = (index + 1) % texts.length; // Passe au texte suivant
+    }
+
+    const typingSpeed = isDeleting ? 50 : 150;
+    setTimeout(type, typingSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    type(); // Lancer l'animation au chargement de la page
+});
