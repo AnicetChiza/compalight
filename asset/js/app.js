6d3551df96a8f91a53cc8a0537ffdd1dae2f7b03
AnicetChiza <chizaanicet@gmail.com>
2024-10-09 16:30:07 +0200

    add to my repository

--- asset/js/app.js
+++ asset/js/app.js
@@ -34,3 +34,26 @@ function type() {
 document.addEventListener("DOMContentLoaded", () => {
     type(); // Lancer l'animation au chargement de la page
 });
+
+
+// 
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector('.scroller-inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
