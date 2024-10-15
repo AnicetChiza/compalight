4adc72de455b0543895271bd51780f314a2d5c05
AnicetChiza <chizaanicet@gmail.com>
2024-10-15 17:11:32 +0200

    add to my repository

--- asset/js/app.js
+++ asset/js/app.js
@@ -57,3 +57,28 @@ function addAnimation() {
         });
     });
 }
+
+
+
+
+
+// 
+document.addEventListener('DOMContentLoaded', function () {
+    const categoryItems = document.querySelectorAll('.list-items ul li');
+    const shoeCategories = document.querySelectorAll('.us');
+
+    categoryItems.forEach(item => {
+        item.addEventListener('click', function () {
+            categoryItems.forEach(catItem => catItem.classList.remove('active'));
+            this.classList.add('active');
+            const filter = this.getAttribute('data-filter');
+            shoeCategories.forEach(category => {
+                if (category.classList.contains(filter)) {
+                    category.style.display = 'flex';
+                } else {
+                    category.style.display = 'none';
+                }
+            });
+        });
+    });
+});
