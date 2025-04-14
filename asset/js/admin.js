1729ab2c53aa976da43f0656a5214f335d5b4311
Anicet Chiza <chizaanicet@gmail.com>
2025-04-14 17:46:18 +0200

    add to my repository

--- asset/js/admin.js
+++ asset/js/admin.js
@@ -25,3 +25,39 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+/*----------------------------------
+#Add member
+----------------------------------*/
+const placeholders = {
+    facebook: "Entrer le lien de Facebook",
+    instagram: "Entrer le lien d'Instagram",
+    whatsapp: "Entrer le lien de WhatsApp",
+    twitter: "Entrer le lien de Twitter",
+    linkedin: "Entrer le lien de LinkedIn",
+  };
+
+  const checkboxContainer = document.getElementById("checkbox-container");
+  const inputsContainer = document.getElementById("inputs-container");
+  const checkboxes = checkboxContainer.querySelectorAll("input[type='checkbox']");
+
+  checkboxContainer.addEventListener("change", () => {
+    // Réinitialiser
+    inputsContainer.innerHTML = "";
+
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        const value = checkbox.value;
+
+        const wrapper = document.createElement("div");
+        wrapper.className = "input-wrapper";
+
+        const input = document.createElement("input");
+        input.type = "url";
+        input.placeholder = placeholders[value] || "Entrer un lien";
+
+        wrapper.appendChild(input);
+        inputsContainer.appendChild(wrapper);
+      }
+    });
+  });
